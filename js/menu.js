4cffefca40b56e953b5fc977614e989378c9b5c7
Anatolii Riabokhlyst <anatoliirst@gmail.com>
2022-02-10 14:05:52 +0200

    hw8-edit-end

--- js/menu.js
+++ js/menu.js
@@ -3,6 +3,7 @@
     openModalBtn: document.querySelector('[data-mob-menu-open]'),
     closeModalBtn: document.querySelector('[data-mob-menu-close]'),
     modal: document.querySelector('[data-mob-menu]'),
+    body: document.querySelector('[body]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
@@ -10,5 +11,6 @@
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
   }
 })();
